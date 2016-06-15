6899fc3a13e112b28da15c16ebc37e0e4af61c7e
Douglas dos Santos Brito <doug.dbrito@gmail.com>
2016-06-15 00:26:44 -0300

    Média ponderada 2

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+  	float media[4], peso[4], mediafim=0, totpeso=0, totmedia=0;
+  	int nota=1;
+  	for(int i=0; i < 4; i++) {
+		  printf("Digite a Nota %d: \n", nota);
+		  scanf("%f", &media[i]);
+		  printf("Digite o peso da Nota %d: \n", nota);
+		  scanf("%f", &peso[i]);
+		  totmedia= totmedia+(media[i]*peso[i]);
+		  totpeso= totpeso+peso[i];
+		  nota++;
+  	}
+  	mediafim= totmedia/totpeso;
+  	printf("Média Final %.2f\n", mediafim);
+}
