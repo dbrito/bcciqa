99eb9acac8b7a6306c2c4a561186a6bb013085b5
Douglas dos Santos Brito <doug.dbrito@gmail.com>
2016-06-15 01:17:45 -0300

    Organização dos Métodos

--- main.c
+++ main.c
@@ -1,19 +1,51 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 main() {
-  	float media[4], peso[4], mediafim=0, totpeso=0, totmedia=0;
-  	int nota=1;
-  	for(int i=0; i < 4; i++) {
-		  printf("Digite a Nota %d: \n", nota);
-		  scanf("%f", &media[i]);
-		  printf("Digite o peso da Nota %d: \n", nota);
-		  scanf("%f", &peso[i]);
-		  totmedia= totmedia+(media[i]*peso[i]);
-		  totpeso= totpeso+peso[i];
-		  nota++;
-  	}
-  	mediafim= totmedia/totpeso;
-  	printf("Média Final %.2f\n", mediafim);
+  	
+}
+
+//Oxigênio Dissolvido, peso 0,17
+oxigenio(){
+	printf("Oxigênio\n");
+}
+
+//Coliformes Fecais, peso 0,15
+coliformes(){
+	printf("Coliformes\n");
+}
+
+//Potencial Hidrogeniônico, peso 0,12
+potencialHidrogenionico(){
+	printf("Potencial Hidrogeniônico\n");
+}
+
+//Demanda Bioquimica de Oxigênio, peso 0,10
+demandaOxigenio(){
+	printf("Demanda Bioquimica de Oxigênio\n");
+}
+
+//Temperatura, peso 0,10
+temperatura(){
+	printf("Temperatura\n");
+}
+
+//Nitrogênio Total, peso 0,10
+nitrogenio(){
+	printf("Nitrogênio\n");
+}
+
+//Fósforo Total, peso 0,10
+fosforo(){
+	printf("Fósforo\n");
+}
+
+//Turbidez, peso 0,08
+turbidez(){
+	printf("Turbidez\n");
+}
+
+//Resíduo Total, peso 0,08
+residuo(){
+	printf("Resíduo Total\n");
 }
